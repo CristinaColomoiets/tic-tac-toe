151f8a3e0b1b5d05b620a339500680de6f9d5256
CristinaColomoiets <colomoetscristina@gmail.com>
2024-07-18 13:14:55 +0200

    finished first logics, actualization of state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 
 const TURNS = {
@@ -5,19 +6,66 @@ const TURNS = {
   O: 'o'
 }
 
-const board = Array(9).fill(null)
+const Square = ({children, isSelected, updateBoard, index, isTurned})=>{
+  
+  const className = `square ${isSelected ? 'is-selected' : ''} ${isTurned ? 'is-turned' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  
+  return(
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )  
+}
 
 
 function App() {
+  
+  const [board, setBoard] = useState(Array(9).fill(null))
+ 
+  const [turn, setTurn] = useState(TURNS.X)
 
+  const updateBoard = (index) =>{
 
-  return (
-    <>
-    <main className='board'>
-      <p className="read-the-docs">tic-tac-toe</p>
-      <section className='game'></section>
-    </main>
-    </>
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+  }
+  
+
+  return ( 
+  <main className='board'>
+
+    <h1 className="read-the-docs">Tic-Tac-Toe</h1>
+
+    <section className='game'>
+      {
+        board.map((_, index)=>{
+          return (
+            <Square 
+              key={index} 
+              index={index}
+              updateBoard = {updateBoard}
+
+            >
+            {board[index]}
+            </Square>
+          )
+        })
+      }
+    </section>
+
+    <section className="turn">
+      <Square isTurned={true} isSelected={turn == TURNS.X}>{TURNS.X}</Square>
+      <Square isTurned={true} isSelected={turn == TURNS.O}>{TURNS.O}</Square>
+    </section>
+  </main>
   )
 }
 
